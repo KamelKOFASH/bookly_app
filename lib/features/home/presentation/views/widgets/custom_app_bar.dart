37712abf1a505035ece01c9b41fd06d32590619d
Mustafa Kamel <mk8764687@gmail.com>
2024-09-28 16:01:19 +0300

    "Updated logo SVG, added font_awesome_flutter dependency, and modified HomeView to use HomeViewBody widget."

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Row(
+          children: [
+            Image.asset(
+              AssetsData.logo,
+              height: 18.h,
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                // TODO: implement search functionality
+              },
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 26,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
+
