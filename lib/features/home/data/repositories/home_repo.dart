62b49ac3d863147bdf214b3d06e2e8dd629f7c75
Mustafa Kamel <mk8764687@gmail.com>
2024-09-29 21:09:56 +0300

    "Updated HomeRepo interface with new method signatures, added dartz dependency to pubspec.yaml and pubspec.lock"

--- lib/features/home/data/repositories/home_repo.dart
+++ lib/features/home/data/repositories/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
