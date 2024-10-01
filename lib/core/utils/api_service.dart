14755eba10d36ca0343f3f1aa455aae41442d631
Mustafa Kamel <mk8764687@gmail.com>
2024-10-01 20:56:48 +0300

    "Updated HomeRepo interface, added dio and dio_web_adapter dependencies, and modified pubspec files accordingly."

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,93 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio _dio; //? _ means private
+
+  ApiService(this._dio);
+
+  //! get request
+  Future<Map<String, dynamic>> get(
+      {required String endPoint, String? token}) async {
+    try {
+      final response = await _dio.get(
+        "$_baseUrl$endPoint",
+        options: Options(
+          headers: token != null ? {"Authorization": "Bearer $token"} : {},
+        ),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception('Failed to load products: ${e.response?.statusCode}');
+    }
+  }
+
+  //! post request
+  Future<dynamic> post(
+      {required String url, required dynamic body, String? token}) async {
+    try {
+      final response = await _dio.post(
+        url,
+        data: body,
+        options: Options(
+          headers: token != null ? {"Authorization": "Bearer $token"} : {},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      throw Exception(
+          'Failed to load products: ${e.response?.statusCode}, ${e.response?.data}');
+    }
+  }
+
+  //! put request
+  Future<dynamic> put(
+      {required String url, required dynamic body, String? token}) async {
+    try {
+      final response = await _dio.put(
+        url,
+        data: body,
+        options: Options(
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            if (token != null) "Authorization": "Bearer $token",
+          },
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      throw Exception('Failed to load products: ${e.response?.statusCode}');
+    }
+  }
+
+  //! delete request
+  Future<dynamic> delete({required String url}) async {
+    try {
+      final response = await _dio.delete(url);
+      return response.data;
+    } on DioError catch (e) {
+      throw Exception('Failed to load products: ${e.response?.statusCode}');
+    }
+  }
+
+  //! patch request
+  Future<dynamic> patch(
+      {required String url, required Map<String, dynamic> body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      return response.data;
+    } on DioError catch (e) {
+      throw Exception('Failed to load products: ${e.response?.statusCode}');
+    }
+  }
+
+  //! head request
+  Future<dynamic> head({required String url}) async {
+    try {
+      final response = await _dio.head(url);
+      return response.data; // Note: HEAD requests usually do not return a body
+    } on DioError catch (e) {
+      throw Exception('Failed to load products: ${e.response?.statusCode}');
+    }
+  }
+}
---
+++ lib/features/home/data/repositories/home_repo_impl.dart
@@ -0,0 +1,35 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repositories/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Sorting=newest&Filtering=free-ebooks&q=subject:programming");
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
